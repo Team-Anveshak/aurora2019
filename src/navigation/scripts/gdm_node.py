819a7f481fde95005df4575754e9bd12b8a402c9
Achu-shankar <achu.shankar2@gmail.com>
2018-12-16 22:17:24 +0530

    fix gps node threading

--- src/navigation/scripts/gdm_node.py
+++ src/navigation/scripts/gdm_node.py
@@ -8,7 +8,7 @@ from termcolor import colored
 from sensor_msgs.msg import NavSatFix
 from navigation.msg import Goal,Planner_state
 from navigation.srv import plan_state
-from threading import Thread
+import thread
 
 
 class GPS() :
@@ -16,12 +16,12 @@ class GPS() :
 		rospy.init_node("gdm")
 		#rospy.wait_for_service('Planner_state_ctrl')
 		self.state_srv = rospy.ServiceProxy('Planner_state_ctrl', plan_state)
-		
+
 		self.pub_goal = rospy.Publisher('goal', Goal,queue_size=10) #Publisher to planner
 		rospy.Subscriber("fix", NavSatFix, self.gpsCallback) #From nmea node
 		rospy.Subscriber("planner_state",Planner_state, self.plannerCallback)
-		
-		'''file_path = "/home/anveshak/aurora2018/src/navigation/config/gps_data.txt"
+
+		file_path = "/home/achu/aurora2019/src/navigation/config/gps_data.txt"
 		try:
 			self.f=open(file_path,'r')
 			self.dest_lat_cont,self.dest_lon_cont = [],[]
@@ -30,8 +30,8 @@ class GPS() :
 				self.dest_lat_cont.append(row[0])
 				self.dest_lon_cont.append(row[1])
 		except Exception:
-			print colored("GPS data file not opened",'red')'''
-			
+			print colored("GPS data file not opened",'red')
+
 		self.dest_lat_cont,self.dest_lon_cont = [],[]
 		print "Enter GPS way-points one by one in latitude<>longitude format"
 		print colored('$ Type ok once done', 'green')
@@ -41,29 +41,30 @@ class GPS() :
 			self.dest_lat_cont.append(row[0])
 			self.dest_lon_cont.append(row[1])
 			l = raw_input('GPSprompt>>>')
-			
+
 		self.curr_lat = 0.0
 		self.curr_lon = 0.0
 		self.bearing=0
 		self.planner_status = 0
 		self.distance = 0
-		
-		
-		
+		thread.start_new_thread( self.key_intrp,())
+
+
+
 	def run(self):
 		goal = Goal()
-		
+
 		while not rospy.is_shutdown():
-			for i in range(len(self.dest_lat_cont)): 
+			for i in range(len(self.dest_lat_cont)):
 				self.dest_lat= float(self.dest_lat_cont[i])
 				self.dest_lon= float(self.dest_lon_cont[i])
 				self.dist_gps,self.bearing=self.cal()
-			
+
 				goal.distance = self.dist_gps
 				goal.bearing = self.bearing
 				self.pub_goal.publish(goal)
 				time.sleep(0.001)
-			
+
 				while(~self.planner_status):
 					try:
 						self.dist_gps,self.bearing=self.cal()
@@ -75,10 +76,10 @@ class GPS() :
 						sys.exit()
 				#ball detecton service
 				self.srv("rst")
-				
+
 			print 'Successfully past all waypoints'
-		
-	
+
+
 	def srv(self,arg):
 		temp = [0,0,0]
 		if(arg == "pause"):
@@ -92,7 +93,7 @@ class GPS() :
 			temp[2] = 1
 		else:
 			return colored('Error','red')
-			
+
 		try:
 			resp = self.state_srv(temp[0],temp[1],temp[2])
 			print "Service response: %s"%resp
@@ -100,7 +101,7 @@ class GPS() :
 		except rospy.ServiceException, e:
 			print "Service call failed: %s"%e
 			return 'Error'
-	
+
 	def cal(self):
 		lon1, lat1, lon2, lat2 = map(radians, [self.curr_lon, self.curr_lat, self.dest_lon, self.dest_lat])
 		dlon = lon2 - lon1
@@ -111,17 +112,17 @@ class GPS() :
 		bearing = atan2(sin(lon2-lon1)*cos(lat2), (cos(lat1)*sin(lat2))-(sin(lat1)*cos(lat2)*cos(lon2-lon1)))
 		bearing = degrees(bearing)
 		return dist_gps,bearing
-		
+
 	def gpsCallback(self,msg):
 		self.curr_lat = msg.latitude
 		self.curr_lon = msg.longitude
 		self.status = msg.status
-		
+
 	def plannerCallback(self,msg):
 		self.planner_status = msg.status
-		
+
 	def key_intrp(self):
-		print "\n Type 'p' to pause ----- 'c' to contin ----- 'r' to rst the Planner \n" 
+		print "\n Type 'p' to pause ----- 'c' to contin ----- 'r' to rst the Planner \n"
 		while True:
         		try:
         			text = raw_input('$GPS_node>>> ')
@@ -136,15 +137,14 @@ class GPS() :
 			except KeyboardInterrupt:
 			  	print 'Stopping....'
 			  	sys.exit()
-	
+
 if __name__ == '__main__':
 	x = raw_input('Do you want to start the node? (y/n) : ')
-	
+
 	gps = GPS()
-	
+
 	#gps.key_intrp()
 	if(x == 'y'):
 		gps.run()
 	else:
 		sys.exit()
-		
