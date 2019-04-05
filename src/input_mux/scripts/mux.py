90e123d5dbea7ef4d5ec9b664b119130ff79d420
Achu-shankar <achu.shankar2@gmail.com>
2019-04-05 21:38:40 +0530

    input mux changes

--- src/input_mux/scripts/mux.py
+++ src/input_mux/scripts/mux.py
@@ -2,7 +2,7 @@
 import rospy
 # from func import *
 from sensor_msgs.msg import Joy
-from man_ctrl.msg import Wheel_rpm_diff
+from man_ctrl.msg import WheelRpm
 from termcolor import colored
 import thread
 
@@ -10,21 +10,22 @@ rospy.init_node("mux")
 n_inputs = 0
 inputs = rospy.get_param('/Input_mux')
 pub={}
-velocity_joy= 1.0
-angular_vel_joy= 0.0
+velocity= 0.0
+angular_vel= 0.0
+active_input = '1'
 
 def init():
     thread.start_new_thread(user_input,())
     try:
         for nodes in inputs["destination"] :
             a = str(nodes)
-            pub[a] = rospy.Publisher(str(inputs["destination"][nodes]["topic_name"]),Wheel_rpm_diff,queue_size=10)
+            pub[a] = rospy.Publisher(str(inputs["destination"][nodes]["topic_name"]),WheelRpm,queue_size=10)
     except Exception,e:
         print e
     try:
         for nodes in inputs["inputs"]:
             topic = inputs["inputs"][nodes]["topic_name"]
-            rospy.Subscriber(topic,Joy,globals()[nodes])
+            rospy.Subscriber(topic,WheelRpm,globals()[nodes])
     except Exception,e:
         print e
 
@@ -35,34 +36,40 @@ def spin():
         rate.sleep()
 
 def user_input():
+    global active_input 
 
     print "Usr input takes"
     while not rospy.is_shutdown():
-        input = raw_input('Enter device number to change drive input: ')
-        if(input=='1'):
+        active_input= raw_input('Enter device number to change drive input: ')
+        if(active_input=='1'):
             print "drive input joystick\n"
-        elif(input=='2'):
+        elif(active_input=='2'):
             print "drive input gui\n"
         else:
             print "Invalid input\n"
 
 def main():
     global velocity_joy
-    vel= Wheel_rpm_diff()
-    vel.forward = velocity_joy
-    print velocity_joy
+    vel=WheelRpm()
+    vel.vel = velocity
+    vel.omega = angular_vel
+    # print velocity_joy
     pub["drive"].publish(vel)
 
 def joy_drive(msg):
-    global velocity_joy, angular_vel_joy
-    velocity_joy=msg.axes[0]
-    angular_vel_joy=msg.axes[1]
+    global velocity, angular_vel,active_input 
+    if active_input == '1':
+        velocity=msg.vel
+        angular_vel=msg.omega
     # print "hey"
 
-def gui_drive(msg):
-    global velocity_joy, angular_vel_joy
-    velocity_gui=1
-    angular_vel_gui= 2
+def planner_drive(msg):
+    global velocity, angular_vel,active_input 
+    if active_input == '2':
+        velocity=msg.vel
+        angular_vel=msg.omega
+        # velocity=1
+        # angular_vel= 2
 
 def joy_arm(msg):
     pass
---
+++ src/man_ctrl/scripts/joy_driver.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Joy
+from man_ctrl.msg import WheelRpm
+
+forward_vel=0.0
+omega=0.0
+m=1
+    
+def main():
+    rospy.init_node("Joy_driver")
+    print "Joy_driver node started"
+    #############################################
+    global forward_vel, omega ,m
+    vel = WheelRpm()
+    #############################################
+    try:
+        pub = rospy.Publisher('joy_drive',WheelRpm,queue_size=10)
+    except Exception as e:
+         print "couldn't publish -error : {}".format(e)
+
+    try:
+        rospy.Subscriber('joy',Joy,joyCallback)
+    except Exception as e:
+        print "couldn't subscribe - error : {}".format(e)
+
+    #############################################
+    rate = rospy.Rate(5)
+    #############################################
+    while not rospy.is_shutdown():
+    
+        if(abs(forward_vel)>0.1 or abs(omega)>0.1):
+            vel.vel = forward_vel*30*m
+            vel.omega = omega*10*m
+        else:
+            vel.vel = 0
+            vel.omega = 0
+    #############################################
+        pub.publish(vel)
+    #############################################
+        rate.sleep()
+###################################################################################
+
+def joyCallback(msg):
+    global forward_vel, omega
+
+    forward_vel = msg.axes[1]
+    omega = msg.axes[2]
+
+    if(msg.buttons[5]==1):
+        if m <5:
+            m = m + 1
+            print("Max rpm is {}".format(m*30))
+    
+    elif(msg.buttons[4]==1):
+        if m >1:
+            m = m - 1
+            print("Max rpm is {}".format(m*30))
+
+######################################################################################
+if __name__=='__main__':
+    main()
